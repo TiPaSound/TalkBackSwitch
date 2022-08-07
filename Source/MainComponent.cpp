0d1882df025527c83501d8d4ed0f305a20da259b
TiPaSound <timo@tipasound.de>
2022-08-07 21:06:58 +0200

    1

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,66 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    m_btnMute.onClick = [this] {OnMuteButton(); };
+    addAndMakeVisible(m_btnMute);
+
+    m_muteState = MuteState::MuteState_Muted;
+
+    setSize (100, 100);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setFont (juce::Font (16.0f));
+    g.setColour (juce::Colours::white);
+    //g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+    
+    int margin = 5;
+    m_btnMute.setBounds(margin, margin, getWidth() - (2*margin), getHeight() - (2*margin));
+}
+
+void MainComponent::OnMuteButton()
+{
+    SwitchMuteOnUi();
+}
+
+void MainComponent::SetMuteStateOnButton()
+{
+    if (MuteState_Muted == m_muteState)
+    {
+        m_btnMute.setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colours::darkseagreen);
+    }
+    else
+    {
+        m_btnMute.setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colours::darkred);
+    }
+}
+
+void MainComponent::SwitchMuteOnUi()
+{
+    if (MuteState_Muted == m_muteState)
+    {
+        // set unmute
+    }
+    else
+    {
+        //set mute
+    }
+}
---
+++ Source/MainComponent.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+
+#define MUTE_CHANNEL    1
+
+
+class MainComponent  : public juce::Component
+{
+public:
+
+    enum MuteState
+    {
+        MuteState_Muted = 1,
+        MuteState_Unmuted
+    };
+    //==============================================================================
+    MainComponent();
+    ~MainComponent() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    //==============================================================================
+    // Your private member variables go here...
+
+    juce::TextButton m_btnMute{"Mute"};;
+    MuteState m_muteState;
+
+    void OnMuteButton();
+    void SetMuteStateOnButton();
+    void SwitchMuteOnUi();
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
